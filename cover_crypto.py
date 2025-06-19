a686fd1bb10a3afb07616edeecd82bfe19959407
theinfallible <162000693+theinfallible@users.noreply.github.com>
2025-06-19 17:02:49 +0530

    Create cover_crypto.py

---
+++ cover_crypto.py
@@ -0,0 +1,23 @@
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.backends import default_backend
+import os
+
+key = os.urandom(32)  # AES-256
+iv = os.urandom(16)   # Initialization vector
+
+def aes_encrypt(plaintext: bytes) -> bytes:
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(plaintext) + padder.finalize()
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    return iv + encryptor.update(padded_data) + encryptor.finalize()
+
+def aes_decrypt(ciphertext: bytes) -> bytes:
+    iv = ciphertext[:16]
+    actual_cipher = ciphertext[16:]
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+    padded_data = decryptor.update(actual_cipher) + decryptor.finalize()
+    unpadder = padding.PKCS7(128).unpadder()
+    return unpadder.update(padded_data) + unpadder.finalize()
