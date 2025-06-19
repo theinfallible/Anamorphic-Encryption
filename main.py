cb07699465a6c84870a0c62d6c4c29dc50914158
theinfallible <162000693+theinfallible@users.noreply.github.com>
2025-06-19 17:03:42 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from cover_crypto import aes_encrypt, aes_decrypt
+from anamorphic_layer import embed_secret_into_cover, extract_secret_from_cover
+
+# Normal encryption
+message = b"This is a normal message"
+cover_ciphertext = aes_encrypt(message)
+
+# Embed secret
+secret_message = "This is a secret hidden from dictator"
+anamorphic_cipher = embed_secret_into_cover(cover_ciphertext, secret_message)
+
+# Decrypting normal message
+print("Decrypted cover message:", aes_decrypt(cover_ciphertext))
+
+# Extracting secret
+print("Hidden message extracted:", extract_secret_from_cover(anamorphic_cipher))
